9a11b11ca9d3faedf08956fc76cc2504c17dca40
AgurSan <clement.sananikone@gmail.com>
2024-05-16 15:27:22 +0200

    axios + fetchGlobalMetrics + doc

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,1 +1,33 @@
-//TODO: fct fetchData qui enverra des requêtes à l'api et qui renverra les reponses de l'api
+import axios from "axios";
+
+const API_URL = 'http://localhost:3000';
+
+interface MonthlyMetric {
+    revenue: number;
+    orders: number;
+}
+
+interface GlobalMetrics {
+    totalRevenue: number;
+    avgRevenuePerOrder: number;
+    uniqueCustomers: number;
+    monthlyMetrics: { [month: string]: MonthlyMetric };
+}
+
+/**
+ * Fetches global metrics based on the year and state provided.
+ *
+ * @param {number} year - The year for which metrics are fetched
+ * @param {string} state - The state for which metrics are fetched
+ * @return {Promise<GlobalMetrics>} The data representing the global metrics
+ */
+export async function fetchGlobalMetrics(year?: number, state?: string): Promise<GlobalMetrics> {
+    const url = `${API_URL}/metrics${year ? `?year=${year}` : ''}${state ? `&state=${state}` : ''}`;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+        throw new Error('Erreur lors de la récupération des données');
+    }
+}
